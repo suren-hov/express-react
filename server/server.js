a1cbce903c1ca164c384b8385c2225ceaab02a6a
suren-hov <shovhannisyan2021@gmail.com>
2024-08-28 10:42:28 +0400

    Added products CR

--- server/server.js
+++ server/server.js
@@ -4,31 +4,32 @@ const port = process.env.PORT || 5000;
 const cors = require('cors');
 const { MongoClient } = require("mongodb")
 const dotenv = require('dotenv')
+const bodyParser = require('body-parser');
 
 app.use(cors());
 dotenv.config();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 const client = new MongoClient(process.env.MONGODB_URI);
 
 (async () => {
-    let conn, db, collection;
-    try {
-        conn = await client.connect();
-        db = conn.db("express-react");
-    } catch(e) {
-        console.error(e);
-    }
-
-    try {
-        collection = await db.collection("posts");
-    } catch(e) {
-        console.error(e);
-    }
+    const conn = await client.connect();
+    const db = conn.db("express-react");
+    let collection = await db.collection("products");
 
     app.get('/', async (req, res) => {
-        const message = await collection.find({}).toArray()
-        res.json({ message: message[0].a})
+        const message = await collection.find({}).toArray();
+        res.json({ message: message })
     });
+
+    app.post('/submit', async (req, res) => {
+        const { name, price } = req.body;
+        const message = await collection.insertOne({name: name, age: price});
+        res.send(`Received data - Name: ${name}, Age: ${price}`);
+    });
+
     app.listen(port, async () => {
         console.log(`Server is running on port ${port}`);
     });
